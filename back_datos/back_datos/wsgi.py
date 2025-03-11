c58e6fcad523b2a0aa1a87f6c019056f884c3dbd
ImJaredGB <jaredalejandro.gb@gmail.com>
2025-03-11 18:43:25 -0500

    wsgi.py añadir sys.path.append

--- back_datos/back_datos/wsgi.py
+++ back_datos/back_datos/wsgi.py
@@ -1,5 +1,8 @@
+import sys
 import os
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_datos.back_datos.settings')
 
