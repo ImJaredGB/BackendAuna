d0a6ca524081fae04817dbe3e7400605ba85f8ac
ImJaredGB <jaredalejandro.gb@gmail.com>
2025-03-11 16:40:26 -0500

    prueba

--- back_datos/back_datos/wsgi.py
+++ back_datos/back_datos/wsgi.py
@@ -5,6 +5,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_datos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_datos.back_datos.settings')
 
 application = get_wsgi_application()
