60c9e923ace1fc45f7d32db34d601c8586952d6d
ImJaredGB <jaredalejandro.gb@gmail.com>
2025-03-11 16:27:20 -0500

    Modificacion al wsgi.py nueva linea

--- back_datos/back_datos/settings.py
+++ back_datos/back_datos/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'back_datos.app_datos',
+    'app_datos',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
